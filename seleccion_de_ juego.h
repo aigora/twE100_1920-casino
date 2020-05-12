239620504e39e8d2de61f018fc77f2a018a5f3f4
TuBro100tifico <61791262+TuBro100tifico@users.noreply.github.com>
2020-05-12 15:37:24 +0200

    Create seleccion_de_ juego.h

---
+++ seleccion_de_ juego.h
@@ -0,0 +1,31 @@
+void Presentacion (void);
+void Presentacion (void){
+	puts ("Bienvenido al casino virtual de la ETSIDI\n Qu\202dese el tiempo y disfrute.");
+}
+char opcion (void);
+char opcion (void) {
+	char letra;
+	puts ("Elija el juego al que quiere jugar");
+	puts ("A. Ruleta");
+	puts ("B. Tragaperras");
+	puts ("C. BlackJack");
+	puts ("D. Poker (Texas Hold'em)");
+	scanf ("%c", &letra);
+	fflush (stdin);
+	return letra;}
+	
+char repeticion (void);
+char repeticion (void){
+	char tecla;
+	puts ("\n\n\250Desea jugar a otra cosa\77");
+	scanf ("%c", &tecla);
+	fflush (stdin);
+	return tecla;
+}
+
+void cerrar (void);
+void cerrar (void)
+{
+	puts ("\n\nPulse \256retorno de carro\257 para cerrar la ventana...");
+	getchar ();
+}
