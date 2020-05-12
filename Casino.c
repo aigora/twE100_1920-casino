02a5809efd5197d8414dee8eac2920a2e504d5fc
TuBro100tifico <61791262+TuBro100tifico@users.noreply.github.com>
2020-05-12 15:37:20 +0200

    Create Casino.c

---
+++ Casino.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include "seleccion_de_ juego.h"
+#include "Ruleta.h"
+#include "Tragaperras.h"
+#include "BlackJack.h"
+#include "Poker.h"
+//probar que funciona
+int main(){
+char rep,juego;
+
+	Presentacion();
+	
+	do{
+	
+	juego= opcion();
+	
+		switch (juego)
+		{
+			case 'A':
+			case 'a':
+			puts ("jaja");		
+				break;
+				
+			case 'B':
+			case 'b':
+				break;
+				
+			case 'C':
+			case 'c':
+				break;
+				
+			case 'D':
+			case 'd':
+				break;	
+			default:
+				puts ("Me temo que aqu\241 no tenemos esa opci\242n se\244or...");
+}
+	rep = repeticion ();
+	} while (rep == 's' || rep == 'S');
+
+	cerrar ();
+	return 0;
+}
+
+
