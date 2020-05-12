3ed05aa9a0ddc1ed727172b498a501519c919011
TuBro100tifico <61791262+TuBro100tifico@users.noreply.github.com>
2020-05-12 16:40:07 +0200

    Create Baraja.h

---
+++ Baraja.h
@@ -0,0 +1,28 @@
+
+typedef struct{
+	int num;
+	std::string palo;
+	int ok;
+} carta;
+
+carta SeleccionCarta (void);
+carta SeleccionCarta (void){
+	carta carta;
+	srand (time (NULL));
+	carta.ok= rand() % 4+1;
+	carta.num= rand () % 12+1;
+	if(carta.ok==1){
+		carta.palo="picas";
+	}
+	else if(carta.ok==2){
+		carta.palo="corazones";
+	}
+	else if (carta.ok==3){
+		carta.palo="treboles";
+	}
+	else{
+		carta.palo="diamantes";
+	}
+	return carta;
+	
+}
