258426bda080197fbb05b3f82f234c37b41d6606
TuBro100tifico <61791262+TuBro100tifico@users.noreply.github.com>
2020-05-16 16:42:59 +0200

    Create Fichas.h

---
+++ Fichas.h
@@ -0,0 +1,20 @@
+int fichas= 500;
+
+int RestarFichas (int fichas, int resta);
+int RestarFichas (int fichas, int resta){
+	int total;
+	total= fichas-resta;
+	return total; 
+}
+
+int SumarFichas (int fichas, int suma);
+int SumarFichas (int fichas, int suma){
+	int total;
+	total= fichas+suma;
+	return total;
+}
+
+void MostrarFichas(int fichas);
+void MostrarFichas(int fichas){
+	printf("\n\nAhora mismo tienes %d fichas\n\n", fichas);
+}
