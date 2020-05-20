388dbd86bcc224b8cbb37f4e2bc1d66b0b810426
TuBro100tifico <61791262+TuBro100tifico@users.noreply.github.com>
2020-05-20 19:47:03 +0200

    Update Ruleta2.cpp

--- Ruleta2.cpp
+++ Ruleta2.cpp
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
- 
+#include "Fichas.H"
 #define DIM 20
  
 char presentacion();
- 
 void espera(int segundos);
+int ApuestaR(void);
  
 int main ()
 {
  char c;
- int n=0,i,premio,k=4;
+ int n=0,i,premio,k=4, apuestaR;
  
 srand(time(NULL));
  
@@ -21,6 +21,7 @@ switch (c) {
  case 'A' :
  case 'a':
  do {
+ 	apuestaR= ApuestaR();
  printf ("1. Apostar a los pares \n"); 
  printf ("2. Apostar a los impares\n");
  scanf ("%d",&n);}while(n!=1 && n!=2);
@@ -29,6 +30,7 @@ switch (c) {
 case 'B' :
 case 'b':
  do {
+ 	apuestaR= ApuestaR();
  printf ("1. Apostar a la primera columna \t (1,4,7,10,13,16,19,22,25,28,31,34)\n");
  printf ("2. Apostar a la segunda columna \t (2,5,8,11,14,17,20,23,26,29,32,35)\n");
  printf ("3. Apostar a la tercera columna \t (3,6,9,12,15,18,21,24,27,30,33,36)\n");
@@ -38,6 +40,7 @@ case 'b':
 case 'C' :
 case 'c':
  do {
+ 	apuestaR= ApuestaR();
 	printf ("1. Apostar falta \t (1-18)\n");
  	printf ("2. Apostar pasa \t (19-36)\n");
 	scanf ("%d",&n);
@@ -47,6 +50,7 @@ case 'c':
  case 'D':
  case 'd':
   do{
+  	apuestaR= ApuestaR();
   	printf ("1. Apostar a la primera docena \t (1,2,3,4,5,6,7,8,9,10,11,12)\n");
  	printf ("2. Apostar a la segunda docena \t (13,14,15,16,17,18,19,20,21,22,23,24)\n");
  	printf ("3. Apostar a la tercera docena \t (25,26,27,28,29,30,31,32,33,34,35,36)\n");
@@ -57,6 +61,7 @@ break;
 case 'E' :
 case 'e':
  do {
+ 	apuestaR= ApuestaR();
  printf ("Elija el numero al que desea apostar \n");
  
 scanf ("%d",&n);
@@ -87,36 +92,63 @@ if (premio==0) {
 switch ( c ) {
  case 'A' :
  case 'a':
- if ((n==1 && premio%2 == 0) || (n==2 && premio%2 == 1))
+ if ((n==1 && premio%2 == 0) || (n==2 && premio%2 == 1)){
  printf ("Enhorabuena, has ganado \n");
- else printf ("Lo siento, has perdido \n");
+ fichas= fichas+ apuestaR*2;
+ MostrarFichas(fichas);
+ }
+ else{
+  printf ("Lo siento, has perdido \n");
+  fichas= RestarFichas(fichas, apuestaR);
+  MostrarFichas(fichas);}
  break;
  
 case 'B' :
 case 'b':
- if ((n==1 && premio%3 == 1) || (n==2 && premio%3 == 2) || (n==3 && premio%2 == 0))
+ if ((n==1 && premio%3 == 1) || (n==2 && premio%3 == 2) || (n==3 && premio%2 == 0)){
  printf ("Enhorabuena, has ganado \n");
- else printf ("Lo siento, has perdido \n");
+  fichas= fichas+ apuestaR*3;
+   MostrarFichas(fichas);
+ }
+ else{
+ printf ("Lo siento, has perdido \n");
+ fichas= RestarFichas(fichas, apuestaR);
+ MostrarFichas(fichas);}
  break;
  
 case 'C' :
 case 'c':
- if ((n==1 && premio%2 == 0) || (n==2 && premio%2 == 1))
+ if ((n==1 && premio-19<0) || (n==2 && premio-19>0)){
  printf ("Enhorabuena, has ganado \n");
- else printf ("Lo siento, has perdido \n");
+  fichas= fichas+ apuestaR*2;
+   MostrarFichas(fichas);}
+ else{
+ printf ("Lo siento, has perdido \n");
+ fichas= RestarFichas(fichas, apuestaR);
+ MostrarFichas(fichas);}
  break;
  
 case 'D' :
 case 'd':
- if ((n==1 && premio%3 == 1) || (n==2 && premio%3 == 2) || (n==3 && premio%2 == 0))
+ if ((n==1 && premio-12<=0) || (n==2 && premio-12>0 && premio-12<=12) || (n==3 && premio-12>12)){
  printf ("Enhorabuena, has ganado\n");
- else printf ("Lo siento, has perdido \n");
+ fichas= fichas+ apuestaR*3;
+  MostrarFichas(fichas);}
+ else{ 
+ printf ("Lo siento, has perdido \n");
+ fichas= RestarFichas(fichas, apuestaR);
+ MostrarFichas(fichas);}
  break;
 case 'E' :
 case 'e':
- if (n==premio)
+ if (n==premio){
  printf ("Enhorabuena, has ganado\n");
- else printf ("Lo siento, has perdido \n");
+ fichas= fichas+ apuestaR*36;
+  MostrarFichas(fichas);}
+ else{
+ printf ("Lo siento, has perdido \n");
+ fichas= RestarFichas(fichas, apuestaR);
+ MostrarFichas(fichas);}
  break;
  
 }
@@ -160,3 +192,11 @@ void espera(int segundos)
  Seg2=(int)clock()/CLOCKS_PER_SEC;
  }
 } 
+
+int ApuestaR(void){
+	int x;
+	printf("¿Cuantas fichas quieres apostar?");
+	scanf("%d", &x);
+	fflush(stdin);
+	return x;
+}
